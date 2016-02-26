5357447fc3a427a4ee092723b2c25ef553a9f753
Ellen <giesygirl4@gmail.com>
2016-02-25 21:50:25 -0800

    Added files via upload

---
+++ Mastermind.js
@@ -0,0 +1,30 @@
+/*var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+ 
+ solutionArray [];
+ 
+	function solution() {
+		while (solutionArray.length < 4) {
+			var randomNumber = math.floor(math.random() * numbers.length);
+			//add randomNumber to solutionArray
+			
+			
+		}
+		
+	};
+*/	
+	
+var answer = [];
+
+var makeAnswer() = function {
+     while answer.length <= 3 {
+     var addToAnswer = math.floor(math.random() * 10);
+     answer.push(addToAnswer);
+     }
+};
+
+
+var playing() = function {
+     promt "Please enter your 4 digit guess"
+	 
+    
+}
